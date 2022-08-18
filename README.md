1f130156a8748da4d3f6b436f55f3c15cd2a3a29
Mike Neilson <m.allan.neilson@gmail.com>
2022-08-17 22:15:51 -0700

    Setup ant project with dependency tracking.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # rest_api
 Rest API that serves OpenDCS database objects as JSON
+
+
+## Target
+
+This web app is targeted at Tomcat and Jetty, as such the Jersey JAX-RS jars will be pulled in with the build.
+You'll need to manually remove these if you try to run the application in a full Java EE Application Server
--- src/opendcs/opentsdb/hydrojson/RestServices.java
+++ src/opendcs/opentsdb/hydrojson/RestServices.java
@@ -2,6 +2,7 @@ package opendcs.opentsdb.hydrojson;
 
 import javax.ws.rs.ApplicationPath;
 import org.glassfish.jersey.server.ResourceConfig;
+import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
 
 import opendcs.opentsdb.hydrojson.errorhandling.WebAppExceptionMapper;
 
@@ -13,6 +14,9 @@ public class RestServices
 	{
         packages("com.fasterxml.jackson.jaxrs.json");
         packages("opendcs.opentsdb.hydrojson");
+		packages("io.swagger.v3.jaxrs2.integration.resources");
         register(WebAppExceptionMapper.class);
+		OpenApiResource openApiResource = new OpenApiResource();
+        register(openApiResource);
 	}
 }
