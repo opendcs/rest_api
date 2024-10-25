4d44bcdf803fa6fdada388c02ce10129d4a09799
Adam Korynta <adam@rmanet.com>
2024-10-25 10:26:03 -0700

    example concept of using opendcs DAO's as a replacement for rest_api dao
    this will gain access to other database implementations and reduce SQL query duplications.
    still needs unit tests
    still needs integration tests
    still needs to use artifacts produced after merging https://github.com/opendcs/opendcs/pull/681

--- opendcs-rest-api/src/main/java/org/opendcs/odcsapi/res/AlgorithmResources.java
+++ opendcs-rest-api/src/main/java/org/opendcs/odcsapi/res/AlgorithmResources.java
@@ -1,7 +1,7 @@
 /*
- *  Copyright 2023 OpenDCS Consortium
+ *  Copyright 2024 OpenDCS Consortium and its Contributors
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  Licensed under the Apache License, Version 2.0 (the "License")
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
  *       http://www.apache.org/licenses/LICENSE-2.0
@@ -16,8 +16,10 @@
 package org.opendcs.odcsapi.res;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.logging.Logger;
-
 import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
@@ -26,26 +28,36 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import decodes.sql.DbKey;
+import decodes.tsdb.ConstraintException;
+import decodes.tsdb.DbAlgoParm;
+import decodes.tsdb.DbCompAlgorithm;
+import decodes.tsdb.DbCompAlgorithmScript;
+import decodes.tsdb.DbIoException;
+import decodes.tsdb.NoSuchObjectException;
+import decodes.tsdb.compedit.AlgorithmInList;
+import opendcs.dai.AlgorithmDAI;
+import org.opendcs.odcsapi.beans.ApiAlgoParm;
 import org.opendcs.odcsapi.beans.ApiAlgorithm;
-import org.opendcs.odcsapi.dao.ApiAlgorithmDAO;
+import org.opendcs.odcsapi.beans.ApiAlgorithmRef;
+import org.opendcs.odcsapi.beans.ApiAlgorithmScript;
 import org.opendcs.odcsapi.dao.DbException;
 import org.opendcs.odcsapi.errorhandling.ErrorCodes;
 import org.opendcs.odcsapi.errorhandling.WebAppException;
-import org.opendcs.odcsapi.hydrojson.DbInterface;
 import org.opendcs.odcsapi.sec.AuthorizationCheck;
 import org.opendcs.odcsapi.util.ApiConstants;
 import org.opendcs.odcsapi.util.ApiHttpUtil;
 
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.toList;
+
 @Path("/")
-public class AlgorithmResources
+public class AlgorithmResources extends OpenDcsResource
 {
-	@Context HttpHeaders httpHeaders;
-	
+
 	@GET
 	@Path("algorithmrefs")
 	@Produces(MediaType.APPLICATION_JSON)
@@ -53,11 +65,30 @@ public class AlgorithmResources
 	public Response getAlgorithmRefs() throws DbException
 	{
 		Logger.getLogger(ApiConstants.loggerName).fine("getAlgorithmRefs");
-		try (DbInterface dbi = new DbInterface();
-			ApiAlgorithmDAO dao = new ApiAlgorithmDAO(dbi))
+		try(AlgorithmDAI dai = getDb().getDao(AlgorithmDAI.class))
 		{
-			return ApiHttpUtil.createResponse(dao.getAlgorithmRefs());
+			List<ApiAlgorithmRef> algorithmRefs = dai.listAlgorithmsForGui()
+					.stream()
+					.map(AlgorithmResources::map)
+					.sorted(comparing(ApiAlgorithmRef::getAlgorithmId))
+					.collect(toList());
+			return ApiHttpUtil.createResponse(algorithmRefs);
 		}
+		catch(DbIoException ex)
+		{
+			throw new DbException(ex, "getAlgorithmRefs error in query");
+		}
+	}
+
+	static ApiAlgorithmRef map(AlgorithmInList algorithm)
+	{
+		ApiAlgorithmRef retval = new ApiAlgorithmRef();
+		retval.setAlgorithmId(algorithm.getAlgorithmId().getValue());
+		retval.setDescription(algorithm.getDescription());
+		retval.setAlgorithmName(algorithm.getAlgorithmName());
+		retval.setExecClass(algorithm.getExecClass());
+		retval.setNumCompsUsing(algorithm.getNumCompsUsing());
+		return retval;
 	}
 
 	@GET
@@ -75,30 +106,91 @@ public class AlgorithmResources
 
 		Logger.getLogger(ApiConstants.loggerName).fine("getAlgorithm algorithmid=" + algoId);
 
-		try(DbInterface dbi = new DbInterface();
-			ApiAlgorithmDAO dao = new ApiAlgorithmDAO(dbi))
+		try(AlgorithmDAI dai = getDb().getDao(AlgorithmDAI.class))
 		{
-			return ApiHttpUtil.createResponse(dao.getAlgorithm(algoId));
+			ApiAlgorithm apiAlgorithm = map(dai.getAlgorithmById(DbKey.createDbKey(algoId)));
+			return ApiHttpUtil.createResponse(apiAlgorithm);
+		}
+		catch(NoSuchObjectException e)
+		{
+			throw new WebAppException(ErrorCodes.NO_SUCH_OBJECT,
+					"No Computation Algorithm with id=" + algoId, e);
+		}
+		catch(DbIoException ex)
+		{
+			throw new DbException(ex, "getAlgorithm error in query");
 		}
 	}
 
-	
+	private ApiAlgorithm map(DbCompAlgorithm algorithm)
+	{
+		ApiAlgorithm retval = new ApiAlgorithm();
+		retval.setAlgorithmId(algorithm.getId().getValue());
+		retval.setName(algorithm.getName());
+		retval.setExecClass(algorithm.getExecClass());
+		retval.setDescription(algorithm.getComment());
+		retval.setNumCompsUsing(algorithm.getNumCompsUsing());
+		retval.setProps(algorithm.getProperties());
+		List<ApiAlgoParm> parameters = new ArrayList<>();
+		Iterator<DbAlgoParm> params = algorithm.getParms();
+		while(params.hasNext())
+		{
+			parameters.add(map(params.next()));
+		}
+		retval.setParms(parameters);
+		retval.setAlgoScripts(algorithm.getScripts().stream()
+				.map(this::map)
+				.collect(toList()));
+		return retval;
+	}
+
+	private ApiAlgorithmScript map(DbCompAlgorithmScript script)
+	{
+		ApiAlgorithmScript retval = new ApiAlgorithmScript();
+		retval.setText(script.getText());
+		retval.setScriptType(script.getScriptType().getDbChar());
+		return retval;
+	}
+
+	private ApiAlgoParm map(DbAlgoParm parameter)
+	{
+		ApiAlgoParm retval = new ApiAlgoParm();
+		retval.setParmType(parameter.getParmType());
+		retval.setRoleName(parameter.getRoleName());
+		return retval;
+	}
+
+
 	@POST
 	@Path("algorithm")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	@RolesAllowed({AuthorizationCheck.ODCS_API_ADMIN, AuthorizationCheck.ODCS_API_USER})
 	public Response postAlgorithm(ApiAlgorithm algo)
-		throws WebAppException, DbException, SQLException
+			throws WebAppException, DbException, SQLException
 	{
 		Logger.getLogger(ApiConstants.loggerName).fine("post algo received algo " + algo.getName()
-			+ " with ID=" + algo.getAlgorithmId());
-		
-		try (DbInterface dbi = new DbInterface();
-			ApiAlgorithmDAO dao = new ApiAlgorithmDAO(dbi))
+				+ " with ID=" + algo.getAlgorithmId());
+		try(AlgorithmDAI dai = getDb().getDao(AlgorithmDAI.class))
 		{
-			return ApiHttpUtil.createResponse(dao.writeAlgorithm(algo));
+			dai.writeAlgorithm(map(algo));
+			return ApiHttpUtil.createResponse(algo);
 		}
+		catch(DbIoException ex)
+		{
+			throw new DbException(ex, "postAlgorithm error in query");
+		}
+	}
+
+	private DbCompAlgorithm map(ApiAlgorithm algo)
+	{
+		DbCompAlgorithm retval = new DbCompAlgorithm(DbKey.createDbKey(algo.getAlgorithmId()),
+				algo.getName(), algo.getExecClass(), algo.getDescription());
+		retval.setNumCompsUsing(algo.getNumCompsUsing());
+		algo.getProps()
+				.forEach((key, value) -> retval.setProperty(String.valueOf(key), String.valueOf(value)));
+		algo.getParms().forEach(p -> retval.addParm(new DbAlgoParm(p.getRoleName(), p.getParmType())));
+		return retval;
 	}
 
 	@DELETE
@@ -107,15 +199,22 @@ public class AlgorithmResources
 	@Produces(MediaType.APPLICATION_JSON)
 	@RolesAllowed({AuthorizationCheck.ODCS_API_ADMIN, AuthorizationCheck.ODCS_API_USER})
 	public Response deletAlgorithm(@QueryParam("algorithmid") Long algorithmId)
-		throws WebAppException, DbException, SQLException
+			throws WebAppException, DbException, SQLException
 	{
 		Logger.getLogger(ApiConstants.loggerName).fine("DELETE algorithm received algorithmId=" + algorithmId);
-		// Use username and password to attempt to connect to the database
-		try (DbInterface dbi = new DbInterface();
-			ApiAlgorithmDAO dao = new ApiAlgorithmDAO(dbi))
+
+		try(AlgorithmDAI dai = getDb().getDao(AlgorithmDAI.class))
 		{
-			dao.deleteAlgorithm(algorithmId);
+			dai.deleteAlgorithm(DbKey.createDbKey(algorithmId));
 			return ApiHttpUtil.createResponse("Algorithm with ID " + algorithmId + " deleted");
+		}
+		catch(ConstraintException ex)
+		{
+			throw new WebAppException(ErrorCodes.NOT_ALLOWED, ex.getMessage(), ex);
+		}
+		catch(DbIoException ex)
+		{
+			throw new DbException(ex, "deleteAlgorithm error in query");
 		}
 	}
 }
