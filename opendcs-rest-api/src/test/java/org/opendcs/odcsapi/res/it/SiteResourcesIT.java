cf4d7941f60f85fa5bc737cf98663c81a25b2b02
zack-rma <zack@rmanet.com>
2024-12-03 11:53:52 -0800

    SiteResources controller updated to use new DAI

---
+++ opendcs-rest-api/src/test/java/org/opendcs/odcsapi/res/it/SiteResourcesIT.java
@@ -0,0 +1,37 @@
+package org.opendcs.odcsapi.res.it;
+
+import javax.servlet.http.HttpServletResponse;
+
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.session.SessionFilter;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.opendcs.odcsapi.fixtures.DatabaseContextProvider;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+@Tag("integration")
+@ExtendWith(DatabaseContextProvider.class)
+final class SiteResourcesIT
+{
+	@TestTemplate
+	void testRoundTrip()
+	{
+		SessionFilter sessionFilter = new SessionFilter();
+		given()
+			.log().ifValidationFails(LogDetail.ALL, true)
+			.accept("application/json")
+			.filter(sessionFilter)
+		.when()
+			.redirects().follow(true)
+			.redirects().max(3)
+			.get("siterefs")
+		.then()
+			.log().ifValidationFails(LogDetail.ALL, true)
+		.assertThat()
+			.statusCode(is(HttpServletResponse.SC_OK))
+		;
+	}
+}
