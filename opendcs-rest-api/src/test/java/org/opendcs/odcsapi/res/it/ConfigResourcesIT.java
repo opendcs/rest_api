f64939117ace8d0ee153b6b4b73a6e2e53055661
zack-rma <zack@rmanet.com>
2024-12-31 16:24:50 -0800

    Initial IT, bug in retrieval discovered - IN PROGRESS

--- opendcs-rest-api/src/test/java/org/opendcs/odcsapi/res/it/ConfigResourcesIT.java
+++ opendcs-rest-api/src/test/java/org/opendcs/odcsapi/res/it/ConfigResourcesIT.java
@@ -1,14 +1,11 @@
 package org.opendcs.odcsapi.res.it;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Properties;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.core.MediaType;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.filter.session.SessionFilter;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.AfterEach;
@@ -16,14 +13,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.opendcs.odcsapi.beans.ApiConfigScript;
-import org.opendcs.odcsapi.beans.ApiConfigScriptSensor;
-import org.opendcs.odcsapi.beans.ApiConfigSensor;
-import org.opendcs.odcsapi.beans.ApiPlatformConfig;
-import org.opendcs.odcsapi.beans.ApiUnitConverter;
 import org.opendcs.odcsapi.fixtures.DatabaseContextProvider;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 
 @Tag("integration")
@@ -32,7 +25,6 @@ final class ConfigResourcesIT extends BaseIT
 {
 	private static SessionFilter sessionFilter;
 	private static Long configId;
-	private static final ObjectMapper mapper = new ObjectMapper();
 
 	@BeforeEach
 	void setUp() throws Exception
@@ -42,43 +34,7 @@ final class ConfigResourcesIT extends BaseIT
 
 		authenticate(sessionFilter);
 
-		ApiPlatformConfig config = new ApiPlatformConfig();
-		config.setDescription("Test Platform");
-		config.setNumPlatforms(1);
-		config.setName("Test Platform");
-		ArrayList<ApiConfigSensor> sensors = new ArrayList<>();
-		ApiConfigSensor sensor = new ApiConfigSensor();
-		sensor.setSensorName("Test Sensor");
-		sensor.setAbsoluteMax(100.0);
-		sensor.setAbsoluteMin(0.0);
-		sensor.setRecordingInterval(900);
-		sensor.setTimeOfFirstSample(0);
-		Properties properties = new Properties();
-		properties.setProperty("site", "Test Site");
-		sensor.setProperties(properties);
-		HashMap<String, String> dataTypes = new HashMap<>();
-		dataTypes.put("Test Type", "Test Units");
-		sensor.setDataTypes(dataTypes);
-		sensors.add(sensor);
-		config.setConfigSensors(sensors);
-		ArrayList<ApiConfigScript> scripts = new ArrayList<>();
-		ApiConfigScript script = new ApiConfigScript();
-		script.setName("Test Script");
-		ArrayList<ApiConfigScriptSensor> scriptSensors = new ArrayList<>();
-		ApiConfigScriptSensor scriptSensor = new ApiConfigScriptSensor();
-		ApiUnitConverter unitConverter = new ApiUnitConverter();
-		unitConverter.setAlgorithm("x * 100 + 32");
-		unitConverter.setFromAbbr("C");
-		unitConverter.setToAbbr("F");
-		unitConverter.setA(100.0);
-		unitConverter.setB(32.0);
-		scriptSensor.setUnitConverter(unitConverter);
-		scriptSensors.add(scriptSensor);
-		script.setScriptSensors(scriptSensors);
-		scripts.add(script);
-		config.setScripts(scripts);
-
-		String configJson = mapper.writeValueAsString(config);
+		String configJson = getJsonFromResource("config_input_data.json");
 
 		ExtractableResponse<Response> response = given()
 			.log().ifValidationFails(LogDetail.ALL, true)
@@ -127,6 +83,8 @@ final class ConfigResourcesIT extends BaseIT
 	@TestTemplate
 	void testGetConfigRefs()
 	{
+		JsonPath expected = getJsonPathFromResource("config_refs_expected.json");
+
 		given()
 			.log().ifValidationFails(LogDetail.ALL, true)
 			.accept(MediaType.APPLICATION_JSON)
@@ -141,12 +99,17 @@ final class ConfigResourcesIT extends BaseIT
 			.log().ifValidationFails(LogDetail.ALL, true)
 		.assertThat()
 			.statusCode(is(HttpServletResponse.SC_OK))
+			.body("[0].name", equalTo(expected.getString("[0].name")))
+			.body("[0].description", equalTo(expected.getString("[0].description")))
+			.body("[0].numPlatforms", equalTo(expected.getInt("[0].numPlatforms")))
 		;
 	}
 
 	@TestTemplate
 	void testGetConfig()
 	{
+		JsonPath expected = getJsonPathFromResource("config_get_expected.json");
+
 		given()
 			.log().ifValidationFails(LogDetail.ALL, true)
 			.accept(MediaType.APPLICATION_JSON)
@@ -162,48 +125,19 @@ final class ConfigResourcesIT extends BaseIT
 			.log().ifValidationFails(LogDetail.ALL, true)
 		.assertThat()
 			.statusCode(is(HttpServletResponse.SC_OK))
+			.body("name", equalTo(expected.getString("name")))
+			.body("description", equalTo(expected.getString("description")))
+			.body("numPlatforms", equalTo(expected.getLong("numPlatforms")))
+			.body("scripts.size()", equalTo(expected.getString("scripts.size()")))
+			.body("configSensors.size()", equalTo(expected.getString("configSensors.size()")))
+			// TODO: Add script and configSensor assertions
 		;
 	}
 
 	@TestTemplate
 	void testPostAndDeleteConfig() throws Exception
 	{
-		ApiPlatformConfig config = new ApiPlatformConfig();
-		config.setDescription("Test Platform 1");
-		config.setNumPlatforms(1);
-		config.setName("Test Platform 2");
-		ArrayList<ApiConfigSensor> sensors = new ArrayList<>();
-		ApiConfigSensor sensor = new ApiConfigSensor();
-		sensor.setSensorName("Test Sensor 1");
-		sensor.setAbsoluteMax(120.0);
-		sensor.setAbsoluteMin(2.0);
-		sensor.setRecordingInterval(90);
-		sensor.setTimeOfFirstSample(10000000);
-		Properties properties = new Properties();
-		properties.setProperty("site", "Test Site 1");
-		sensor.setProperties(properties);
-		HashMap<String, String> dataTypes = new HashMap<>();
-		dataTypes.put("Test Type", "Test Units 1");
-		sensor.setDataTypes(dataTypes);
-		sensors.add(sensor);
-		config.setConfigSensors(sensors);
-		ArrayList<ApiConfigScript> scripts = new ArrayList<>();
-		ApiConfigScript script = new ApiConfigScript();
-		script.setName("Test Script");
-		ArrayList<ApiConfigScriptSensor> scriptSensors = new ArrayList<>();
-		ApiConfigScriptSensor scriptSensor = new ApiConfigScriptSensor();
-		ApiUnitConverter unitConverter = new ApiUnitConverter();
-		unitConverter.setAlgorithm("x * A");
-		unitConverter.setFromAbbr("m");
-		unitConverter.setToAbbr("ft");
-		unitConverter.setA(3.28084);
-		scriptSensor.setUnitConverter(unitConverter);
-		scriptSensors.add(scriptSensor);
-		script.setScriptSensors(scriptSensors);
-		scripts.add(script);
-		config.setScripts(scripts);
-
-		String configJson = mapper.writeValueAsString(config);
+		String configJson = getJsonFromResource("config_post_delete_input_data.json");
 
 		ExtractableResponse<Response> response = given()
 			.log().ifValidationFails(LogDetail.ALL, true)
@@ -224,6 +158,32 @@ final class ConfigResourcesIT extends BaseIT
 		;
 
 		Long newConfigId = response.body().jsonPath().getLong("configId");
+
+		JsonPath expected = getJsonPathFromResource("config_post_delete_expected.json");
+
+		// Get the config and assert it matches expected JSON
+		given()
+			.log().ifValidationFails(LogDetail.ALL, true)
+			.accept(MediaType.APPLICATION_JSON)
+			.contentType(MediaType.APPLICATION_JSON)
+			.header("Authorization", authHeader)
+			.filter(sessionFilter)
+			.queryParam("configid", newConfigId)
+		.when()
+			.redirects().follow(true)
+			.redirects().max(3)
+			.get("config")
+		.then()
+			.log().ifValidationFails(LogDetail.ALL, true)
+		.assertThat()
+			.statusCode(is(HttpServletResponse.SC_OK))
+			.body("name", equalTo(expected.getString("name")))
+			.body("description", equalTo(expected.getString("description")))
+			.body("numPlatforms", equalTo(expected.getLong("numPlatforms")))
+			.body("scripts.size()", equalTo(expected.getString("scripts.size()")))
+			.body("configSensors.size()", equalTo(expected.getString("configSensors.size()")))
+			// TODO: Add script and configSensor assertions
+		;
 
 		given()
 			.log().ifValidationFails(LogDetail.ALL, true)
