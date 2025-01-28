da7d004ddf3fa6f463397ffb63a042a4e9c239d4
Adam Korynta <adam@rmanet.com>
2025-01-28 14:49:16 -0800

    add test user to user admins for CAC verification abilities

--- opendcs-rest-api/src/test/java/org/opendcs/odcsapi/fixtures/DatabaseSetupExtension.java
+++ opendcs-rest-api/src/test/java/org/opendcs/odcsapi/fixtures/DatabaseSetupExtension.java
@@ -156,11 +156,19 @@ public class DatabaseSetupExtension implements BeforeEachCallback
 		{
 			String userPermissions = "begin execute immediate 'grant web_user to " + System.getProperty("DB_USERNAME") + "'; end;";
 			String dbOffice = System.getProperty("DB_OFFICE");
+			String setWebUserPermissions = "begin\n" +
+					"   cwms_sec.add_user_to_group(?, 'CWMS User Admins',?) ;\n" +
+					"   commit;\n" +
+					"end;";
 			try(Connection connection = DriverManager.getConnection(System.getProperty("DB_URL"), "CWMS_20",
 					System.getProperty("DB_PASSWORD"));
-				PreparedStatement preparedStatement = connection.prepareStatement(userPermissions))
+				PreparedStatement stmt1 = connection.prepareStatement(userPermissions);
+				PreparedStatement stmt2 = connection.prepareStatement(setWebUserPermissions))
 			{
-				preparedStatement.executeQuery();
+				stmt1.executeQuery();
+				stmt2.setString(1, System.getProperty("DB_USERNAME"));
+				stmt2.setString(2, dbOffice);
+				stmt2.executeQuery();
 			}
 			catch(SQLException e)
 			{
